b51e730f95f7f9a4faf0b6055f830b5fe09836ca
20Hanan <hanansayed2002@gmail.com>
2026-03-29 16:37:20 +0200

    Refactor Map component to improve readability and structure; add ChangeCenter and DetectClick functions for better map interaction; update flag rendering logic

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,57 +1,83 @@
-import { useSearchParams, useNavigate } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useState } from 'react'
-import {useCities} from '../contexts/CitiesContext'
+import { useSearchParams, useNavigate } from "react-router-dom";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
-import styles from './Map.module.css'
+import styles from "./Map.module.css";
 
 function Map() {
-  const [mapPosition,setMapPosition]=useState([40,0]);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat');
-    const lng =searchParams.get('lng');
-    const navigate=useNavigate();
-    const {cities}=useCities();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng],
+  );
+  const flagemojiToPNG = flag => {
+    if (!flag) return null;
 
-    const flagemojiToPNG = flag => {
-      if (!flag) return null;
-
-      var countryCode = Array.from(flag, codeUnit => codeUnit.codePointAt())
-        .map(char => String.fromCharCode(char - 127397).toLowerCase())
-        .join("");
-      return (
-        <img
-          src={`https://flagcdn.com/24x18/${countryCode}.png`}
-          alt="flag"
-        />
-      );
-    };
+    var countryCode = Array.from(flag, codeUnit => codeUnit.codePointAt())
+      .map(char => String.fromCharCode(char - 127397).toLowerCase())
+      .join("");
     return (
-      <div
-        className={styles.mapContainer}
-       
-      >
-        <MapContainer
-          className={styles.map}
-          center={mapPosition}
-          zoom={13}
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-          />
-          {cities.map((city)=>
-          <Marker position={[city.position.lat,city.position.lng]} key={city.id}>
-           
-            <Popup>
-             <span>{flagemojiToPNG(city.emoji)}</span><span>{city.cityName}</span>
-            </Popup>
-          </Marker>)}
-          
-        </MapContainer>
-      </div>
+      <img
+        src={`https://flagcdn.com/24x18/${countryCode}.png`}
+        alt="flag"
+      />
     );
+  };
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        // center={[mapLat||40,mapLng||0]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map(city => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{flagemojiToPNG(city.emoji)}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
+    </div>
+  );
 }
-
-export default Map
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: e => {
+      navigate(`Form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
+}
+export default Map;
