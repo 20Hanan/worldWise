ee02d5ccf8d8ed4fa895daa04008c25181822d9a
20Hanan <hanansayed2002@gmail.com>
2026-05-25 16:33:03 +0300

    Fix import statement for Button component in Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import { useAuth } from "../contexts/FakeAuthContext";
 import { useNavigate } from "react-router-dom";
-import Button from './../components/Button';
+import Button from '../components/Button';
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
